9522a0ed3df78b440ccf2ba0ee01b76c0d3d659d
huifeng <2916865614@qq.com>
2025-11-20 14:13:23 +0800

    11.20

--- rlproject/src/cv/camerastream.py
+++ rlproject/src/cv/camerastream.py
@@ -2,8 +2,10 @@ import cv2
 
 
 class CameraStream:
-    def __init__(self, src=0, width=640, height=480, fps=30):
-        self.cap = cv2.VideoCapture(src)
+    def __init__(self,  width=640, height=480):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 
 
